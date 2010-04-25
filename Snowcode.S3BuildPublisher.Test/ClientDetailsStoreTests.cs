b906fea35b376522b0005f61af7fa050b626beb1
BookSwapSteve <BookSwapSteve@eb82f096-58cc-11de-8832-3f4c773c73ae>
2010-04-25 01:35:48 +0000

    Second attempt to add tests, hopefully using the right folder this time...
    git-svn-id: https://snowcode.googlecode.com/svn/trunk/2010-04-TramDepot/Snowcode.S3BuildPublisher@82 eb82f096-58cc-11de-8832-3f4c773c73ae

---
+++ Snowcode.S3BuildPublisher.Test/ClientDetailsStoreTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using NUnit.Framework;
+
+namespace Snowcode.S3BuildPublisher.Test
+{
+    [TestFixture]
+    public class ClientDetailsStoreTests
+    {
+        private const string DefaultTestRegistrySubKey = "Software\\SnowCode\\S3BuildPublisher\\Tests";
+
+        [SetUp]
+        public void Setup()
+        {
+            if (Registry.CurrentUser.OpenSubKey(DefaultTestRegistrySubKey) != null)
+            {
+                Registry.CurrentUser.DeleteSubKey(DefaultTestRegistrySubKey);
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Registry.CurrentUser.OpenSubKey(DefaultTestRegistrySubKey) != null)
+            {
+                Registry.CurrentUser.DeleteSubKey(DefaultTestRegistrySubKey);
+            }
+        }
+
+        [Test]
+        public void SaveAndLoadClientDetails_AreCorrectlyStored()
+        {
+            const string containerName = "S3BuildPublisher.TestContainer.ClientDetailsStore";
+            var clientDetails = new AwsClientDetails
+                                    {
+                                        AwsAccessKeyId = "AwsAccessKeyId",
+                                        AwsSecretAccessKey = "AwsSecretAccessKey"
+                                    };
+
+            var store = new ClientDetailsStore(DefaultTestRegistrySubKey);
+
+            store.Save(containerName, clientDetails);
+
+            AwsClientDetails actual = store.Load(containerName);
+
+            Assert.AreEqual(clientDetails.AwsAccessKeyId, actual.AwsAccessKeyId, "AwsAccessKeyId");
+            Assert.AreEqual(clientDetails.AwsSecretAccessKey, actual.AwsSecretAccessKey, "AwsSecretAccessKey");
+        }
+    }
+}
---
+++ Snowcode.S3BuildPublisher.Test/EncryptionHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Snowcode.S3BuildPublisher.Test
+{
+    [TestFixture]
+    public class EncryptionHelperTests
+    {
+        [Test]
+        public void EncryptAndDecrpytPassword_ReturnsSamePassword()
+        {
+            const string containerName = "S3BuildPublisher.TestContainer.EncryptionHelper";
+            const string password = "someRandomText1234567890-Passwor!d$$";
+
+            string encrypted = EncryptionHelper.Encrypt(containerName, password);
+            string actual = EncryptionHelper.Decrypt(containerName, encrypted);
+
+            Assert.AreEqual(password, actual);
+        }
+    }
+}
