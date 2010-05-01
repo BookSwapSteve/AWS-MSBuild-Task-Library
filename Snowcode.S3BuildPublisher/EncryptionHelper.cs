f8f191a13a34828efb14337f63b96c316666685c
a.hemmings@goblinfactory.co.uk <a.hemmings@goblinfactory.co.uk@eb82f096-58cc-11de-8832-3f4c773c73ae>
2010-05-01 20:59:17 +0000

    Added very simple salting of password + included missing NUnit assembly.
    git-svn-id: https://snowcode.googlecode.com/svn/trunk/2010-04-TramDepot/Snowcode.S3BuildPublisher@87 eb82f096-58cc-11de-8832-3f4c773c73ae

--- Snowcode.S3BuildPublisher/EncryptionHelper.cs
+++ Snowcode.S3BuildPublisher/EncryptionHelper.cs
@@ -12,11 +12,11 @@ namespace Snowcode.S3BuildPublisher
         /// <param name="containerName">the container</param>
         /// <param name="toEncrypt">The clear value to encrypt</param>
         /// <returns>returns a base 64 encoded string</returns>
-        public static string Encrypt(string containerName, string toEncrypt)
+        public static string Encrypt(string password, string toEncrypt)
         {
             byte[] dataToEncrypt = Encoding.ASCII.GetBytes(toEncrypt);
 
-            var parameters = new CspParameters {KeyContainerName = containerName};
+            var parameters = new CspParameters {KeyContainerName = ContainerName(password)};
 
             // Create a new instance of the RSACryptoServiceProvider class 
             var cryptoServiceProvider = new RSACryptoServiceProvider(parameters);
@@ -28,15 +28,25 @@ namespace Snowcode.S3BuildPublisher
             return Convert.ToBase64String(encryptedData);
         }
 
+        private static string ContainerName(string password)
+        {
+            // ADH: 2010.05.01 
+            // since this is our own private server, and any "attacker" won't have access to the encrypt method
+            // this is sufficient we don't need to HASH the password, and don't need a new salt for each value we will 
+            // be encrypting.
+            return string.Format("{0}{1}", "91A827BB-8D82-490C-80BB-198F080B8C31", password);
+        }
+
+
         /// <summary>
         /// Decrypts the value passed in.
         /// </summary>
         /// <param name="containerName">The container name storing the </param>
         /// <param name="toDecrypt">The encrypted value to decrypt.  Base64 encoded</param>
         /// <returns></returns>
-        public static string Decrypt(string containerName, string toDecrypt)
+        public static string Decrypt(string password, string toDecrypt)
         {
-            var parameters = new CspParameters {KeyContainerName = containerName};
+            var parameters = new CspParameters {KeyContainerName = ContainerName(password) };
             var cryptoServiceProvider = new RSACryptoServiceProvider(parameters);
 
             // Pass the data to ENCRYPT and boolean flag specifying no OAEP padding.
