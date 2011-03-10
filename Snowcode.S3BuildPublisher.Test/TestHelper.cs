2db7b5207a0aaa7c59fe16c32bd25376d461d708
Stephen.Harrison@AnalysisUK.com <Stephen.Harrison@AnalysisUK.com>
2011-03-10 03:37:41 +0000

    Added extra DeleteQueue method to help tests.
    Added common EncryptionContainerName

--- Snowcode.S3BuildPublisher.Test/TestHelper.cs
+++ Snowcode.S3BuildPublisher.Test/TestHelper.cs
@@ -1,9 +1,15 @@
-﻿using Snowcode.S3BuildPublisher.SQS;
+﻿using System;
+using System.Diagnostics;
+using Snowcode.S3BuildPublisher.Logging;
+using Snowcode.S3BuildPublisher.SQS;
 
 namespace Snowcode.S3BuildPublisher.Test
 {
     static class TestHelper
     {
+        //public const string EncryptionContainerName = "MySecretContainer";
+        public const string EncryptionContainerName = "BuildMachineNet";
+
         /// <summary>
         /// Create a queue on AWS to use for testing.
         /// </summary>
@@ -12,13 +18,34 @@ namespace Snowcode.S3BuildPublisher.Test
         /// <returns></returns>
         public static string CreateQueue(string queueName, string encryptionContainerName)
         {
-            var createSqsQueueTask = new CreateSQSQueueTask
+            var createSqsQueueTask = new CreateSQSQueueTask(new AwsClientFactory(), new NullLogger())
                                          {
                                              QueueName = queueName,
                                              EncryptionContainerName = encryptionContainerName
                                          };
             createSqsQueueTask.Execute();
+
+            Debug.WriteLine("TestHelper created SQS Queue: " + createSqsQueueTask.QueueUrl);
+
             return createSqsQueueTask.QueueUrl;
+        }
+
+        /// <summary>
+        /// Delete the queue used for testing
+        /// </summary>
+        /// <param name="queueUrl"></param>
+        /// <param name="encryptionContainerName"></param>
+        public static void DeleteQueue(string queueUrl, string encryptionContainerName)
+        {
+            var createSqsQueueTask = new DeleteSQSQueueTask(new AwsClientFactory(), new NullLogger())
+            {
+                QueueUrl = queueUrl,
+                EncryptionContainerName = encryptionContainerName
+            };
+
+            createSqsQueueTask.Execute();
+
+            Debug.WriteLine("Delted SQS Queue: " + createSqsQueueTask.QueueUrl);
         }
     }
 }
